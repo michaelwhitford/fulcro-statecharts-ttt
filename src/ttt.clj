c199af754d988ad659313a94f1ecfc9111de2284
Michael Whitford <michael@whitford.us>
2023-03-04 19:01:50 -0700

    adjusting naming

--- src/ttt.clj
+++ src/ttt.clj
@@ -32,6 +32,11 @@
                   (transition {:event  :reset
                                :target id})))))
 
+(def tic-tac-toe
+  (statechart {}
+              (parallel {}
+                        (ttt-cell :c0 :empty))))
+
 (def onoff
   (statechart {}
               (parallel {}
@@ -43,42 +48,35 @@
                                (state {:id :off}
                                       (transition {:event  :turn-on
                                                    :target :on}))))))
-
-(def ttt
-  (statechart {}
-              (parallel {}
-                        (ttt-cell :c0 :empty))))
-
 (defn show-states [wmem] (println (sort (::sc/configuration wmem))))
 
 (def env (simple/simple-env))
 
-(simple/register! env ::ttt ttt)
+(simple/register! env ::ttt tic-tac-toe)
 (simple/register! env ::toggle onoff)
 
 (def processor (::sc/processor env))
 
-(def s0 (sp/start! processor env ::ttt {::sc/session-id 1}))
-(def s1 (sp/process-event! processor env s0 (new-event :toggle-o)))
-(def s2 (sp/process-event! processor env s1 (new-event :toggle-x)))
-(def s3 (sp/process-event! processor env s2 (new-event :empty)))
+(def t0 (sp/start! processor env ::ttt {::sc/session-id 1}))
+(def t1 (sp/process-event! processor env t0 (new-event :toggle-o)))
+(def t2 (sp/process-event! processor env t1 (new-event :toggle-x)))
+(def t3 (sp/process-event! processor env t2 (new-event :empty)))
 
-(def t0 (sp/start! processor env ::toggle {::sc/session-id 2}))
-(def t1 (sp/process-event! processor env t0 (new-event :turn-off)))
-(def t2 (sp/process-event! processor env t1 (new-event :turn-on)))
-(def t3 (sp/process-event! processor env t2 (new-event :turn-on)))
-(def t4 (sp/process-event! processor env t3 (new-event :turn-off)))
+(def s0 (sp/start! processor env ::toggle {::sc/session-id 2}))
+(def s1 (sp/process-event! processor env s0 (new-event :turn-off)))
+(def s2 (sp/process-event! processor env s1 (new-event :turn-on)))
+(def s3 (sp/process-event! processor env s2 (new-event :turn-on)))
+(def s4 (sp/process-event! processor env s3 (new-event :turn-off)))
 
 (comment
   (show-states s0)
   (show-states s1)
   (show-states s2)
   (show-states s3)
+  (show-states s4)
   (show-states t0)
   (show-states t1)
   (show-states t2)
-  (show-states t3)
-  (show-states t4)
   (pprint t2)
   (show-states t3)
   (pprint env)
